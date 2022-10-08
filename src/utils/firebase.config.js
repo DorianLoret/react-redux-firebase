6ae814dba6831a9411e2e6f95ff05458d0fec316
Dorian Loret <dorian.loret20@gmail.com>
2022-10-08 19:33:37 +0200

    connexion

---
+++ src/utils/firebase.config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "react-firebase-redux-283f8.firebaseapp.com",
+  projectId: "react-firebase-redux-283f8",
+  storageBucket: "react-firebase-redux-283f8.appspot.com",
+  messagingSenderId: "560213572692",
+  appId: "1:560213572692:web:d53833e6f60523d29e01c1",
+});
+
+export const auth = app.auth();
+export default app;
